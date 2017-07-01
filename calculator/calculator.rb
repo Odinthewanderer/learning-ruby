02db37292d605c3435043503a7f7ea27315b09e1
Joshua Webster <jrwebster@Joshuas-iMac.local>
2017-07-01 10:17:00 -0500

    Added calculator functions

--- calculator/calculator.rb
+++ calculator/calculator.rb
@@ -1,1 +1,25 @@
 #! /usr/bin/ruby
+
+def Add(num1, num2)
+  return num1+num2
+end
+
+def Sub(num1, num2)
+  return num1-num2
+end
+
+def Mult(num1, num2)
+  return num1*num2
+end
+
+def Div(num1, num2)
+  return num1/num2
+end
+
+number1 = 112.0
+number2 = 41.0
+
+puts Add(number1, number2)
+puts Sub(number1, number2)
+puts Mult(number1, number2)
+puts Div(number1, number2)
